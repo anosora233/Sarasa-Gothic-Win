dc30f53c77778750c64f72523c811ccde61d4995
anosora233 <86921203+anosora233@users.noreply.github.com>
2024-05-10 16:28:30 +0800

    Add convert.py

---
+++ convert.py
@@ -0,0 +1,55 @@
+from os import environ
+from pathlib import Path
+from dataclasses import dataclass
+
+from fontTools.ttLib import TTCollection
+
+YAHEI_PATH = Path(environ.get("YAHEI_PATH", "C:\\Windows\\Fonts"))
+
+
+@dataclass
+class Task:
+    input: str
+    output: str
+    template: str
+
+
+def convert(main, temp):
+    main["name"] = temp["name"]
+    main["gasp"] = temp["gasp"]
+
+    main["OS/2"].usWinAscent = temp["OS/2"].usWinAscent
+    main["OS/2"].usWinDescent = temp["OS/2"].usWinDescent
+
+    if main["OS/2"].usWeightClass == 300:
+        main["OS/2"].usWeightClass = 290
+
+    main["GSUB"].table.ScriptList.ScriptRecord = [
+        R for R in main["GSUB"].table.ScriptList.ScriptRecord if R.ScriptTag != "hani"
+    ]  # Remove LangSys `hani` to avoid slow rendering
+
+
+for task in [
+    Task(
+        input="out/TTC/Sarasa-Light.ttc",
+        output="msyhl.ttc",
+        template=YAHEI_PATH / "msyhl.ttc",
+    ),
+    Task(
+        input="out/TTC/Sarasa-Regular.ttc",
+        output="msyh.ttc",
+        template=YAHEI_PATH / "msyh.ttc",
+    ),
+    Task(
+        input="out/TTC/Sarasa-Bold.ttc",
+        output="msyhbd.ttc",
+        template=YAHEI_PATH / "msyhbd.ttc",
+    ),
+]:
+    sara = TTCollection(task.input)
+    msyh = TTCollection(task.template)
+
+    for m in [0, 1]:
+        convert(sara.fonts[m], msyh.fonts[m])
+
+    sara.save(task.output)
